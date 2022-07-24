b8c907ca27bc0f9a1cb63019b17dd22cee8bbb17
Gabriel <omnifarter@gmail.com>
2022-07-25 00:07:33 +0800

    update roles

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,13 +1,13 @@
 export const playerInfo = [
     {
         name:'Bhil Tee',
-        role:'Offlane • Co-founder of Firejet',
-        quote: 'Co-founder of Firejet.',
+        role:'Offlane • Co-founder of Firejet • Let-end Specialist',
+        quote: 'Pre-tips for the offlane.',
         avatar:'https://avatars.akamai.steamstatic.com/eded3ec7295238e304e2c1cb6337c083b71406b4_full.jpg'
     },
     {
         name:'Wokiez',
-        role:'Midlaner • Data Scientist Expert',
+        role:'Midlaner • Boyfriend Hugger',
         quote: "Baoers in.",
         avatar:'https://avatars.akamai.steamstatic.com/6b76134cdc4730ca73b82c8edb6804f32bb0f279_full.jpg'
     },
